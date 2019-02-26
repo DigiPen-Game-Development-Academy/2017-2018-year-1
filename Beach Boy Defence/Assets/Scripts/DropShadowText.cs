b9261bb7051883389a8fd733f96190c5aba49874
wanna flex CrAmBerry <45608790+cloaklinkril@users.noreply.github.com>
2019-02-26 11:59:29 -0800

    format coins text

---
+++ Beach Boy Defence/Assets/Scripts/DropShadowText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropShadowText : MonoBehaviour
+{
+    public Text textToCopy;
+
+    Text ourText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        ourText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (textToCopy)
+        {
+            ourText.text = textToCopy.text;
+        }
+            
+	}
+}
